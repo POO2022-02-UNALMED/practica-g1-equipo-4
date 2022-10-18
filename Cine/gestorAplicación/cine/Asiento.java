b69d2e8866a513b11ec08b5e6f686360333ee96c
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2022-10-18 12:58:25 -0500

    Update Asiento.java

--- Cine/gestorAplicación/cine/Asiento.java
+++ Cine/gestorAplicación/cine/Asiento.java
@@ -1,53 +1,116 @@
-package Cine.gestorAplicación.cine;
-
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Asiento {
-  
-  private String Letra;
-  
-  private int Numero;
-  
-  static Scanner entrada = new Scanner(System.in);
-  
-  public static void main(String[] args) {
-	  
-	  int t = 1, fila, columna;
-	  
-	  int [][] asiento = new int [10][20];
-	  
-	  while (t <= 200) {
-		  
-		  System.out.println( "Ingrese el número de fila (1-10)");
-		  
-		  fila = entrada.nextInt();
-		  
-		  System.out.println( "Ingrese el número de columna (1-20)");
-		  
-		  columna = entrada.nextInt();
-		  
-		  if (asiento [fila - 1] [columna - 1] == 0) {
-			  
-			  asiento [fila - 1] [columna - 1] = 1;
-			  
-			  t = t + 1;
-			  
-			  System.out.println( "Reserva exitosa");			  
-			  
-		  }else
-			  System.out.println( "Asiento ocupado");
-		  		  		  
-	  }
-	  for (int i = 0; i < 10; i++) {
-		  
-		  for (int j = 0; j < 20; j++)
-			  
-			  System.out.println(asiento[i][j] + "\t");
-		  
-		  System.out.println();
-	  }
-	  
-  }
-  
+	
+	private boolean[] asientos;
+	 
+	public Asiento() {
+		
+		asientos = new boolean[200];
+		
+		for (int i = 0; i < 200; i++) {
+			
+			asientos[i] = false;
+		}
+		
+	}
+	
+	public boolean[] get_asientos() {
+		
+		return asientos;
+		
+	}
+	
+	public String Comprar_asiento(String n_asiento) {
+		
+		String[] a = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		for (String e: a) {
+			
+			lista.add(e);
+			
+		}
+		
+		String inicial = n_asiento.substring(0,1);
+		
+		int numeros =  Integer.parseInt(n_asiento.substring(1));
+		
+		int posicion = numeros - 1 + lista.indexOf(inicial)*20;
+		
+		if (asientos[posicion] == false) {
+			
+			asientos[posicion] = true;
+					
+			return "Asiento " + n_asiento + " comprado con éxito.";
+			
+		}
+		
+		else {
+			
+			return "Asiento " + n_asiento + " ocupado.";
+			
+		}
+		
+	}
+	
+	public String Comprar_varios_asientos(String x) {
+		
+		String[] boleta = x.split(", ");
+		
+		boolean q = true;
+		
+		ArrayList<String> falla = new ArrayList<String>();
+		
+		for (String e: boleta) {
+			
+			String inicial = e.substring(0,1);
+			
+			int numeros =  Integer.parseInt(e.substring(1));
+			
+			int posicion = numeros - 1 + e.indexOf(inicial)*20;
+			
+			if (asientos[posicion] == true) {
+				
+				q = false;
+				
+				falla.add(e);
+			}
+			
+		}
+		
+		if (q) {
+			for (String e: boleta) {
+				
+				Comprar_asiento(e);
+				
+			}
+			
+			return "Asientos " + x + " confirmados, gracias por su compra.";
+			
+		}
+		
+		else if (falla.size() == 1){
+			
+			return "No es posible reservar el asiento " + falla.get(0) + "; ya se encuentra ocupado.";
+			
+		}
+		
+		else {
+			
+			String w = falla.get(0);
+			
+			for (int i = 1; i < falla.size(); i++) {
+				
+				w = w + ", " + falla.get(i);
+				
+			}
+			
+			return "No es posible reservar los asientos " + w + "; ya se encuentran ocupados.";
+			
+		}
+		
+	}  
    
 }
