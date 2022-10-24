11c8b4c64471951c8d4df76e9b6af20eae172a99
Julián Orrego Martínez <70676228+AionChronos@users.noreply.github.com>
2022-10-23 20:30:29 -0500

    Update dia.java

---
+++ Cine/gestorAplicación/cine/dia.java
@@ -0,0 +1,35 @@
+package gestorAplicación.cine;
+
+import java.util.ArrayList; 
+
+public class dia{
+
+    private String nombreDelDiaDeLaSemana;
+    private ArrayList<Sala> salasDisponibles=new arraylist<Sala>();
+
+
+    public dia(String nombreDelDiaDeLaSemana, ArrayList<Sala> salasDisponibles){
+        this.nombreDelDiaDeLaSemana=nombreDelDiaDeLaSemana;
+        this.salasDisponibles=salasDisponibles;
+    }
+
+    public String getnombreDelDiaDeLaSemana(){
+        return this.nombreDelDiaDeLaSemana;
+    
+    public void setnombreDelDiaDeLaSemana(String nombreDelDiaDeLaSemana){
+        this.nombreDelDiaDeLaSemana=nombreDelDiaDeLaSemana;
+    }
+
+    public ArrayList<Sala> getSalasDisponibles(){
+        return this.salasDisponibles;
+    }
+
+    public void setSalasDisponibles(ArrayList<Sala> salasDisponibles){
+        this.salasDisponibles=salasDisponibles;
+    } 
+
+
+
+
+
+}
