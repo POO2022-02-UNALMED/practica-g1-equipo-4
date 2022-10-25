987ab9cc2acdeadab26ed02ee9f348621b019c36
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2022-10-25 01:01:28 -0500

    Update Serializador.java

--- Cine/baseDatos/Serializador.java
+++ Cine/baseDatos/Serializador.java
@@ -7,23 +7,43 @@ import gestorAplicación.cine.*;
 import IU.*;
 
 public class Serializador {
+
+		private static File ruta = new File("Cine\\baseDatos\\temp"); //definimos la ruta de los archivos donde se serializaran los objetos
 		
-	public static void serializar() {
-		
-		try {
-            FileOutputStream f = new FileOutputStream(new File(System.getProperty("user.dir")+"\\Cine\\baseDatos\\temp\\Usuario.txt"));
-            ObjectOutputStream o = new ObjectOutputStream(f);
-            o.writeObject(null);
-            o.close();
-            f.close();
-        				
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		public static void serializar() {
+			
+			FileOutputStream fos;
+			
+			ObjectOutputStream oos;
+			
+			File[] docs = ruta.listFiles();
+			
+			PrintWriter pw;
+			
+			for (File file: docs) //Iteramos en los archivos que hallan en la ruta para vaciarlos creando un printwiter
+			{
+				try
+				{
+					pw = new PrintWriter(file);
+				}
+				catch (FileNotFoundException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			
+			for(File file: docs) {
+				if (file.getAbsolutePath().contains("Usuario")) {
+					try {
+						fos = new FileOutputStream(file);
+						oos = new ObjectOutputStream(fos);
+						oos.writeObject(null);
+						}catch (FileNotFoundException e) {
+							e.printStackTrace();
+						}catch (IOException e) {
+							e.printStackTrace();
+						}
+				}
+			}
 		}
-		
-	}
-}
+}			
